434f72c48796bdf3771ffd673f83378f63ceeafd
Serhii <sergeidatsiy@gmail.com>
2026-08-20 14:57:21 +0300

    Autoscaling

--- main.tf
+++ main.tf
@@ -14,18 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["amazon"] # Amazon Linux 2023 (Bitnami community AMIs were discontinued)
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -109,4 +97,27 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_group_arn = aws_lb_target_group.blog.arn
   target_id        = aws_instance.blog.id
   port             = 80
+}
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.3.0"
+
+  name = "blog"
+
+  min_size         = 1
+  max_size         = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  launch_template_name = "blog"
+  security_groups     = [module.blog_sg.id]
+  instance_type = var.instance_type
+  image_id           = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    blog-alb = {
+      target_group_arn = aws_lb_target_group.blog.arn
+    }
+  }
 }
