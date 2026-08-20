87ac09b14b97044cd6bab752a8ee7e67f594e9bd
Serhii <sergeidatsiy@gmail.com>
2026-08-20 13:43:19 +0300

    Fix

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
