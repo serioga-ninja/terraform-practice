de55affa9cb0ee6ec7cbdef404d614f871dc96ed
Serhii <sergeidatsiy@gmail.com>
2026-08-20 15:10:19 +0300

    Refactor to ue variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,24 +11,24 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["amazon"] # Amazon Linux 2023 (Bitnami community AMIs were discontinued)
+  owners = var.ami_filter.owners # Amazon Linux 2023 (Bitnami community AMIs were discontinued)
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -82,7 +82,7 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -99,8 +99,8 @@ module "blog_autoscaling" {
 
   name = "blog"
 
-  min_size         = 1
-  max_size         = 2
+  min_size         = var.min_size
+  max_size         = var.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Filter for the AMI to use for the EC2 instance"
+  type = object({
+    name   = string
+    owners = list(string)
+  })
+
+  default = {
+    name   = "al2023-ami-2023.*-x86_64"
+    owners = ["amazon"] # Amazon Linux 2023 (Bitnami community AMIs were discontinued)
+  }
+
+}
+
+variable "environment" {
+  description = "Environment name"
+  type = object({
+    name = string
+    network_prefix = string
+  })
+  default = {
+    name            = "dev"
+    network_prefix  = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in the Auto Scaling group"
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the Auto Scaling group"
+  default     = 2
+}
